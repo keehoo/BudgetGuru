9e6263a0847268ac618aee557e65c4962ccfb3d3
keehoo <kkubicki2@gmail.com>
2017-06-30 23:17:56 +0200

    Rest

---
+++ src/main/java/com/kree/keehoo/budgetguru/REST/RestApp.java
@@ -0,0 +1,12 @@
+package com.kree.keehoo.budgetguru.REST;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by Krzysiek on 2017-06-30.
+ */
+@ApplicationPath("/api")
+public class RestApp extends Application {
+}
