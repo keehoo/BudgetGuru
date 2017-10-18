ac3494345d9c5f74f5d9272a290fca21c40bebe1
Krzysiek Kubicki <kkubicki2@gmail.com>
2017-10-18 21:56:25 +0200

    Added new login Vaadin UI

---
+++ src/main/java/com/kree/keehoo/budgetguru/Servlets/VaadinUi/LoginUi.java
@@ -0,0 +1,15 @@
+package com.kree.keehoo.budgetguru.Servlets.VaadinUi;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.cdi.CDIUI;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+@Theme("mytheme")
+@CDIUI("login")
+public class LoginUi extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
